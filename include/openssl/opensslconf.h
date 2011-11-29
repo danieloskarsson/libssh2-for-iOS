23b9285a3aef1cfadb1a18df48d569102d992edb
Daniel Oskarsson <daniel.oskarsson@gmail.com>
2011-11-29 14:59:30 +0100

    Removed references to libgcrypt

--- include/openssl/opensslconf.h
+++ include/openssl/opensslconf.h
@@ -71,8 +71,8 @@
 
 #if !(defined(VMS) || defined(__VMS)) /* VMS uses logical names instead */
 #if defined(HEADER_CRYPTLIB_H) && !defined(OPENSSLDIR)
-#define ENGINESDIR "/Users/x2on/Projects/iOS/OpenSSL-for-iPhone/bin/iPhoneSimulator4.2.sdk/lib/engines"
-#define OPENSSLDIR "/Users/x2on/Projects/iOS/OpenSSL-for-iPhone/bin/iPhoneSimulator4.2.sdk"
+#define ENGINESDIR "/Users/Daniel/Repositories/libssh2ios/bin/iPhoneSimulator5.0-i386.sdk/lib/engines"
+#define OPENSSLDIR "/Users/Daniel/Repositories/libssh2ios/bin/iPhoneSimulator5.0-i386.sdk"
 #endif
 #endif
 
